1bd54085f1bfd620fbaf2fbc57899d7ef69b88b8
Hoang <minh.hoang@silverlakeaxis.com>
2022-02-15 15:12:21 +0700

    Update HelloController.java
    asdsad

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "Hello World RESTful with Spring Boot";
+        return "Hello World RESTful with Spring Boot 1";
     }
 }
